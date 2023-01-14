23e002131d27e150aae75f1a8691f086f4bf9034
YangGwangSeong <rhkdtjd_12@naver.com>
2023-01-14 09:37:17 -0800

    feat: 로그인 기능 구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/sequelize';
 import { AuthDto } from './dto/auth.dto';
 import { UserModel } from './users.model';
 
+import { compare, genSalt, hash } from 'bcryptjs';
 @Injectable()
 export class AuthService {
 	constructor(
@@ -12,13 +13,40 @@ export class AuthService {
 		private readonly jwtService: JwtService,
 	) {}
 
-	async login(dto: AuthDto) {}
+	async login(dto: AuthDto) {
+		const user = await this.validateUser(dto);
 
-	async register(dto: AuthDto) {}
+		return {
+			user: this.returnUserFields(user),
+			accessToken: await this.issueAccessToken(String(user.id)),
+		};
+	}
 
 	async validateUser(dto: AuthDto) {
 		const user = await this.userModel.findOne({
 			where: { email: dto.email },
+			attributes: ['id', 'email', 'password'],
 		});
+		if (!user) throw new UnauthorizedException('User not found');
+
+		const isValidPassword = await compare(dto.password, user.password);
+		if (!isValidPassword) throw new UnauthorizedException('Invalid password');
+
+		return user;
+	}
+
+	async issueAccessToken(userId: string) {
+		const data = { id: userId };
+
+		return await this.jwtService.signAsync(data, {
+			expiresIn: '31d',
+		});
+	}
+
+	returnUserFields(user: UserModel) {
+		return {
+			id: user.id,
+			email: user.email,
+		};
 	}
 }
